13201b41d358976831598cc340dde00bdf57edef
https://github.com/princestevezy <stevendozie995@gmail.com>
2026-04-05 00:39:05 +0100

    Updated website design and fixed bugs

--- src/Webapp/About/About.jsx
+++ src/Webapp/About/About.jsx
@@ -1,17 +1,81 @@
 import React from "react";
 import Navbar1 from "../../Components/Navbar/Navbar";
-import { Container, Row, Col } from "react-bootstrap";
-import Button from 'react-bootstrap/Button';
-import four1 from '../../Images/Images/Home/4f.jpg';
+import { Container, Row, Col, FormGroup } from "react-bootstrap";
+import fi from '../../Images/Images/About/Fi.png';
+import Logo from "../../Images/Components/Navbar/four.png";
+import { Link } from "react-router-dom";
 import './About.css';
 
 function About() {
   return (
     <div>
-      <Navbar1 about={{borderBottom:'3px solid red'}}/>
+      <Navbar1 about={{ borderBottom: '3px solid red' }} />
+      <div class='about_banner d-flex justify-content-center align-items-center text-center'>
+        <br />
+        <br />
+        <Container>
+          <font className='text-warning fs-1'>ABOUT</font> <font className='text-white fs-1'>US</font>
+          <br />
+          <img src={fi} style={{ width: "150px" }}></img>
+          <br />
+          <font className='text-white fs-6'><Link to="#" className='text-primary'>Home</Link> /About Us</font>
+        </Container>
       </div>
+      <br />
+      <Container>
+        <p id='p1' className="text-center">Foursquare began as a local church in 1923, and we’ve been serving people to send ever since. We strive to develop “more and
+          growing leaders together on mission.” Learn more about where we are going and how we’re getting there.</p>
+        <br />
+        <br />
+        <video controls style={{ width: "70%", height: "300px", display: "block", marginLeft: "auto", marginRight: "auto" }}>
+          <source src="https://res.cloudinary.com/dgsarpq5w/video/upload/v1700000000/video1_ky8l7n.mp4" type="video/mp4" />
+        </video>
+        <br />
+        <br />
+        <br />
+        <div>
+          <h3 className="text-center text-black" id='h21'>Supporting leaders</h3>
+          <p className="text-center" id="p1">We are committed to supporting leaders in their personal and spiritual growth, providing resources and
+            opportunities for development, and fostering a culture of mentorship and collaboration.</p>
+        </div>
+        <br />
+        <br />
+      </Container>
+      <div class='invitation_banner'>
+        <Container>
+          <br />
+          <h3 classname='text-lg' id="h21" style={{ color: "white" }}>Invitation to Sunday Worship Service</h3>
+          <p id='p1'>
+            In the middle of today’s busy and demanding world, it can be difficult to find moments of peace, hope, and spiritual connection.
+            That is why Foursquare Church warmly invites you to join us this Sunday for a refreshing time of worship, fellowship, and encouragement.
+            <br />
+            <p style={{color:"red"}}>Join us this Sunday for our 10:00 AM church Service </p>
+              <br/>
+          </p>
+          <h3 classname='text-lg' id="h21" style={{ color: "white" }}> Tuesday Weekly Service Invitation </h3>
+          <p id='p1'>
+            In the middle of a busy week, it is important to pause, reflect, and reconnect with God. Our Tuesday Weekly Service at Foursquare Church
+            provides a special opportunity to step away from daily pressures and spend meaningful time in God’s presence.
+            <br />
+              <p style={{color:"red"}}>Join us this Tuesday for our 7:00 PM church Service </p>
+          </p>
+          
+          <h3 classname='text-lg' id="h21" style={{ color: "white" }}> Thursday Weekly Service Invitation </h3>
+          <p id='p1'>
+            As the week moves forward, it is important to take time to renew your spirit and draw closer
+            to God. Our Thursday Weekly Service at Foursquare Church is a special time set aside for worship, prayer, and spiritual growth.
+            <br />
+              <p style={{color:"red"}}>Join us this Thursday for our 7:00 PM church Service </p>
+          </p>
+        </Container>
+      </div>
+      <br />
+      <br />
+      <br />
+    </div>
 
-  
-)
+
+  )
 }
 export default About
+
--- src/Webapp/Home/Home.js
+++ src/Webapp/Home/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Navbar1 from "../../Components/Navbar/Navbar";
-import { Container, Row, Col} from "react-bootstrap";
-import  Card  from "react-bootstrap/Card";
+import { Container, Row, Col } from "react-bootstrap";
+import Card from "react-bootstrap/Card";
 import Button from 'react-bootstrap/Button';
 import four1 from '../../Images/Images/Home/4f.jpg';
 import pastor from '../../Images/Images/Home/pastor.png';
+import t_c from '../../Images/Images/Home/c.jpg';
+import sb from '../../Images/Images/Home/sb.jpg';
+import sm from '../../Images/Images/Home/sm.jpg';
+import Carousel from 'react-bootstrap/Carousel';
 import './Home.css';
 
 
@@ -14,8 +18,7 @@ function Home() {
   const imageUrl3 = `https://res.cloudinary.com/dgsarpq5w/image/upload/image3_gt7dhs.avif;`
   return (
     <div>
-      <Navbar1 home={{borderBottom:'3px solid red'}}/>
-      <div class='cover'>
+      <Navbar1 home={{ borderBottom: '3px solid red' }} />
         <div class='banner d-flex justify-content-center align-items-center text-center'>
           <br />
           <br />
@@ -29,122 +32,177 @@ function Home() {
             </Button>
           </Container>
         </div>
-      </div>
       <br />
-       <Container id="tv">
+      <Container id="tv">
         <div className="d-flex align-items-center justify-content-center">
           <Row>
             <Col xs={12} md={6}>
               <img className='img1' src={four1}></img>
             </Col>
-            <Col xs={12} md={6}>          
-            <font id="h51">The church</font>
-           <font id="h21">Giving is an action of worship, affection and
-             love for Jesus.</font>
-             <br/>
-              <div className="border-bottom border-2 border-danger" style={{width:"50%"}} ></div>  
-              <br/>
-               <p id='p1'>We are a church that believes in Jesus, a church that loves God and people. Overwhelmed by the gift of salvation we have found in Jesus, we have a heart for 
+            <Col xs={12} md={6}>
+              <font id="h51">The church</font>
+              <font id="h21">Giving is an action of worship, affection and
+                love for Jesus.</font>
+              <br />
+              <div className="border-bottom border-2 border-danger" style={{ width: "50%" }} ></div>
+              <br />
+              <p id='p1'>We are a church that believes in Jesus, a church that loves God and people.
+                 Overwhelmed by the gift of salvation we have found in Jesus,
+                 we have a heart for
                 authentic worship, are passionate about the church, and are on
-                mission to see God’s kingdom established across the earth. </p>         
+                mission to see God’s kingdom established across the earth. </p>
             </Col>
-          </Row>      
+          </Row>
         </div>
-         <br/>
-          <br/>
-      </Container>  
-      <br/>
+        <br />
+        <br />
+      </Container>
+      <br />
       <div class='banner1 align-items-center text-center'>
-       <br/>
-       <h3>Upcoming Programs</h3>
-       <br/>
-       <Container>
-      <Row> 
-      <Col xs={12} md={4}> 
-      <Card  className="bg-dark w-65 h-65">
-      <Card.Img variant="top" src={`${imageUrl1}?v=${Date.now()}`} />
-      <Card.Body>
-        <Card.Title className="fs-5">Card Title</Card.Title>
-        <Card.Text style={{ fontSize: "15px" }} className="text-white">
-          Upcoming program
-        </Card.Text>
-      </Card.Body>
-    </Card>
-    </Col>
-    <Col xs={12} md={4}>
-      <Card  className="bg-dark">
-      <Card.Img variant="top" src={`${imageUrl2}?v=${Date.now()}`}/>
-      <Card.Body className="bg-dark">
-        <Card.Title className="fs-5">Card Title</Card.Title>
-        <Card.Text style={{ fontSize: "15px" }} className="text-white">
-          Upcoming program
-        </Card.Text>
-      </Card.Body>
-    </Card>
-    </Col>
-    <Col xs={12} md={4}>
-      <Card  className="bg-dark">
+        <br />
+        <h2><font class='text-warning'>Upcoming</font> <font class='text-white'>Events</font></h2>
+        <br />
+        <Container>
+          <Carousel indicators={false} style={{ color: "red" }}>
+            <Carousel.Item>
+              <Row>
+                <Col xs={12} md={4}>
+                  <Card className="bg-dark mb-4">
+                    <Card.Img variant="top" src={`${imageUrl1}?v=${Date.now()}`} />
+                    <Card.Body>
+                      <Card.Title className="fs-5 text-white"> Upcoming Event </Card.Title>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col xs={12} md={4}>
+                  <Card className="bg-dark mb-4">
+                    <Card.Img variant="top" src={`${imageUrl2}?v=${Date.now()}`} />
+                    <Card.Body className="bg-dark">
+                      <Card.Title className="fs-5 text-white"> Upcoming Event </Card.Title>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col xs={12} md={4}>
+                  <Card className="bg-dark mb-4">
+                    <div>
+                      <Card.Img src={`${imageUrl3}?v=${Date.now()}`} />
+                    </div>
+                    <Card.Body className="bg-dark">
+                      <Card.Title className="fs-5 text-white"> Upcoming Event </Card.Title>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              </Row>
+            </Carousel.Item>
+            <Carousel.Item>
+              <Row>
+                <Col xs={12} md={4}>
+                  <Card className="bg-dark mb-4">
+                    <Card.Img variant="top" src={`${imageUrl1}?v=${Date.now()}`} />
+                    <Card.Body>
+                      <Card.Title className="fs-5 text-white"> Upcoming Event </Card.Title>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col xs={12} md={4}>
+                  <Card className="bg-dark mb-4">
+                    <Card.Img variant="top" src={`${imageUrl2}?v=${Date.now()}`} />
+                    <Card.Body className="bg-dark">
+                      <Card.Title className="fs-5 text-white"> Upcoming Event </Card.Title>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col xs={12} md={4}>
+                  <Card className="bg-dark mb-4">
+                    <div>
+                      <Card.Img src={`${imageUrl3}?v=${Date.now()}`} />
+                    </div>
+                    <Card.Body className="bg-dark">
+                      <Card.Title className="fs-5 text-white"> Upcoming Event </Card.Title>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              </Row>
+            </Carousel.Item>
+          </Carousel>
+        </Container>
+      </div>
+      <br />
+      <br />
       <div>
-        <Card.Img src={`${imageUrl3}?v=${Date.now()}`} />
-      </div>
-      <Card.Body className="bg-dark">
-        <Card.Title className="fs-5">Card Title</Card.Title>
-        <Card.Text style={{ fontSize: "15px" }} className="text-white">
-          Upcoming program
-        </Card.Text>
-      </Card.Body>
-    </Card>
-    </Col>
-    </Row>
-    </Container>
-      </div>
-       <br/>
-    <br/>
-     <br/>
-    <br/>
-    <br/>
-        <div>
-          <Container>
+        <Container>
           <Row className="justify-content-center">
-            <Col md={4}>          
-           <font id="h21">PASTOR
-            <br/>
-           <font className='' style={{fontSize:"19px"}}>MRS HELEN DOZIE</font>
-            </font>  
-              <br/>
-              <div className="border-bottom border-2 border-danger" style={{width:"10%"}} ></div>  
-              <br/>
-               <p id='p1'style={{width:"100%"}}>We are a church that believes in Jesus, a church that loves God and people. Overwhelmed by the gift of salvation we have found in Jesus, we have a heart for 
-                authentic worship, are passionate about the church, and are on
-                mission to see God’s kingdom established across the earth. </p>         
-            </Col>
             <Col md={4}>
-              <img className='img1' src={pastor} style={{width:"300px",height:"400px"}}></img>
+              <font id="h21">PASTOR
+                <br />
+                <font className='' style={{ fontSize: "19px" }}>MRS HELEN DOZIE</font>
+              </font>
+              <br />
+              <div className="border-bottom border-2 border-danger" style={{ width: "10%" }} ></div>
+              <br/>
+              <p id='p1' style={{ width: "100%" }}>We are a church that believes in Jesus, a church that loves God and people. Overwhelmed by the gift of salvation we have found in Jesus, we have a heart for
+                authentic worship, are passionate about the church, and are on
+                mission to see God’s kingdom established across the earth. </p>
             </Col>
-          </Row>   
-          </Container>
-        </div>
-         <br/>
+            <Col md={4} className="d-flex justify-content-center align-items-center">
+              <img  src={pastor} style={{width: "70%", height: "350px" }}></img>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <br/>
+       <br/>
         <br/>
-        <br/>
-        
-
-    <br/>
-  </div>
+       <br/>
+      <Container className="mb-8">
+        <br />
+        <h3 className="text-lg h mb-8">Our Mission</h3>
+        <br />
+        <Row>
+          <Col xs={12} md={4}>
+            <Card className="bg-white w-65 h-65 mb-4">
+              <Card.Img variant="top" src={t_c} style={{ width: "100%", height: "200px" }} />
+              <Card.Body>
+                <Card.Title className="fs-5 text-black ct">Teach Children</Card.Title>
+                <Card.Text style={{ fontSize: "15px" }} className="text-black" id='p1'>
+                  To educate children biblical lessons in a caring and encouraging setting so they can comprehend God's Word,
+                  develop spiritually, and form moral character based on Christian values.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col xs={12} md={4}>
+            <Card className="bg-white w-65 h-65 mb-4">
+              <Card.Img variant="top" src={sb} style={{ width: "100%", height: "200px" }} />
+              <Card.Body>
+                <Card.Title className="fs-5 text-black ct">Study the Bible</Card.Title>
+                <Card.Text style={{ fontSize: "15px" }} className="text-black" id='p1'>
+                  To promote regular Bible study among adults, helping them gain knowledge of God’s Word,
+                  strengthen their faith, and apply biblical principles in
+                  their personal and spiritual lives.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col xs={12} md={4}>
+            <Card className="bg-white w-65 h-65 mb-4">
+              <Card.Img variant="top" src={sm} style={{ width: "100%", height: "200px" }} />
+              <Card.Body>
+                <Card.Title className="fs-5 text-black ct"> Service and Ministry </Card.Title>
+                <Card.Text style={{ fontSize: "15px" }} className="text-black" id='p1'>
+                  To empower and inspire individuals to faithfully serve God by utilizing their spiritual
+                  talents, abilities, and gifts to strengthen the church and have an impact on the community.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+      <br />
+    </div>
   );
 }
 export default Home;
-
-
-
-
-
-
-
-
-
-
-
 
 
 
